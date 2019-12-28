6e110c9e321bac792ec1f932d490c564fd15bf14
Nathan Martindale <nathanamartindale@gmail.com>
2019-12-28 08:04:19 -0600

    Add server instance to game class

--- catbox/game.py
+++ catbox/game.py
@@ -6,12 +6,16 @@
 
 class Game():
 
-    def __init__():
+    def __init__(self, server):
+        self.server = server
+        
         self.code = "AAAA"
         self.players = {}
 
+        # TODO: queue for each player if they disconnect (add to queue if username sid is none, then send all when it is no longer onone)
 
-    def add_player(username, sid):
+
+    def add_player(self, username, sid):
         if username in self.players.keys():
             if self.players[username] is not None:
                 # TODO: error message
