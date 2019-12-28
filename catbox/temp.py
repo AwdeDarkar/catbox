bcf20e5d09d0e5d0a09a6d612210fb7f50ce2fa5
Nathan Martindale <nathanamartindale@gmail.com>
2019-12-28 08:02:08 -0600

    Abstract part of flask handling into class

--- catbox/temp.py
+++ catbox/temp.py
@@ -5,18 +5,35 @@ client_sids = []
 
 # NOTE: unclear where below should go
 socketio = SocketIO(app)
+server = Server(socketio)
 
 @socketio.on('connect')
 def connect():
-    client_sids.append(request.sid)
+    # client_sids.append(request.sid)
+    server.handle_connect(request)
 
 
 @socketio.on('disconnect')
 def disconnect():
-    client_sids.remove(request.sid)
+    # client_sids.remove(request.sid)
     # TODO: remove sid from game connected to
+    server.handle_disconnect(request)
 
 
 # TODO: this should be in some class if possible?
-def communicate(sid, event, data):
-    socketio.emit(event, data, room=sid)
+
+
+class Server():
+
+    def __init__(self, socketio):
+        self.socketio = socketio
+        self.client_sids = []
+            
+    def handle_connect(self, request):
+        self.client_sids.append(request.sid)
+    
+    def handle_disconnect(self, request):
+        self.client_sids.remove(request.sid)
+        
+    def communicate(sid, event, data):
+        self.socketio.emit(event, data, room=sid)
