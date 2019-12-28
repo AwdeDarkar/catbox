1dedd4b742a473e411f1e5134b41905ed2fab679
Nathan Martindale <nathanamartindale@gmail.com>
2019-12-28 07:49:37 -0600

    Add server communicate function

--- catbox/temp.py
+++ catbox/temp.py
@@ -3,10 +3,18 @@ from flask_socketio import SocketIO
 
 client_sids = []
 
+# NOTE: unclear where below should go
+socketio = SocketIO(app)
+
 @socketio.on('connect')
 def connect():
     client_sids.append(request.sid)
 
+
 @socketio.on('disconnect')
 def disconnect():
     client_sids.remove(request.sid)
+
+
+def communicate(sid, event, data):
+    socketio.emit(event, data, room=sid)
