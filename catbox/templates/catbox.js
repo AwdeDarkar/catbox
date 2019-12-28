3e64a54fa924e9443954f91bb60cdc2d81b88acb
Nathan Martindale <nathanamartindale@gmail.com>
2019-12-28 10:28:37 -0600

    Create join form and handler on client side

--- catbox/templates/catbox.js
+++ catbox/templates/catbox.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+var socket;
+var connected = false;
+
+
+function ConnectSocket()
+{
+	socket = io.connect('http://' + document.domain + ':' + location.port);
+	socket.on("connect", function() { connected = true; });
+	// TODO: error handling if connection goes wrong?
+}
+
+
+function ConnectionFormHandler(e)
+{
+	e.preventDefault();
+	code = $("#connection_code").val();
+	username = $("#connection_code").val();
+	socket.emit("join game", {
+		code: code,
+		username: username 
+	});
+}
+
+
+function OnPageLoad()
+{
+	ConnectSocket();
+	var form = $("connection_form").on("submit", ConnectionFormHandler);
+	$("#connection_username").val("").focus();
+}
